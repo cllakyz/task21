4e6d21a8fea8c9e9c016235b276939439fef073b
Celal <celal7174@gmail.com>
2021-01-23 15:09:37 +0300

    Unit test complete

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Collection;
 
 class Task extends Model
 {
@@ -25,8 +27,65 @@ class Task extends Model
         'status',
     ];
 
-    public function assignedUser()
+    /**
+     * Assigned user
+     *
+     * @return BelongsTo
+     */
+    public function assignedUser(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * sortByStatus func
+     *
+     * @return Collection
+     */
+    public static function sortByStatus(): Collection
+    {
+        $doingArr = [];
+        $todoArr = [];
+        $doneArr = [];
+        $tasks = Task::all();
+        foreach ($tasks as $task) {
+            if ($task->status === Task::DOING) {
+                $doingArr[] = $task;
+            } elseif ($task->status === Task::TODO) {
+                $todoArr[] = $task;
+            } else {
+                $doneArr[] = $task;
+            }
+        }
+
+        return collect(array_merge(self::quickSort($doingArr), self::quickSort($todoArr), self::quickSort($doneArr)));
+    }
+
+    /**
+     * Quick Sort function
+     *
+     * @param $data
+     * @return array
+     */
+    private static function quickSort($data): array
+    {
+        $left = [];
+        $right = [];
+
+        if (count($data) < 2) {
+            return $data;
+        }
+
+        $pivot_key = key($data);
+        $pivot = array_shift($data);
+
+        foreach ($data as $d) {
+            if ($d->title <= $pivot->title) {
+                $left[] = $d;
+            } elseif ($d->title > $pivot->title) {
+                $right[] = $d;
+            }
+        }
+        return array_merge(self::quickSort($left), [$pivot_key => $pivot], self::quickSort($right));
     }
 }
